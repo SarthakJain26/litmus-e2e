28cc6aba0049d9d0d8b21939ca677281d3889e28
Vedant Shrotria <vedant.shrotria@mayadata.io>
2020-09-09 20:51:48 +0530

    Doing
    Signed-off-by: Vedant Shrotria <vedant.shrotria@mayadata.io>

--- uninstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Stopping the microk8s cluster ......................................"
-microk8s stop
-
-echo "Removing the microk8s .................................."
-sudo snap remove microk8s
