36fc7d9dd3639af4d577604733990f556abacd14
Vedant Shrotria <vedant.shrotria@mayadata.io>
2020-09-15 19:32:35 +0530

    Testing
    Signed-off-by: Vedant Shrotria <vedant.shrotria@mayadata.io>

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
+#!/bin/bash
+set -e
+path=$(pwd)
+
 kubectl apply -f https://raw.githubusercontent.com/litmuschaos/litmus/master/litmus-portal/k8s-manifest.yml
 kubectl wait --for=condition=Ready pods --all -n litmus
 kubectl patch svc litmusportal-frontend-service -p '{"spec": {"type": "LoadBalancer"}}' -n litmus
-export EXTERNAL_IP=$(kubectl get svc litmusportal-frontend-service -n litmus -o jsonpath="{.status.loadBalancer.ingress[0].ip}")
-cat <<< "$EXTERNAL_IP" > "external_ip.txt"
+
+# Caching the External_IP of loadBalancer
+
+echo "export FRONTEND_IP=$(kubectl get svc litmusportal-frontend-service -n litmus -o jsonpath="{.status.loadBalancer.ingress[0].ip}")" >> build.env
+echo $FRONTEND_IP
