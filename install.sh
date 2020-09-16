e0a77c9edd489a384c5abeb33c51abfee15997b7
Vedant Shrotria <vedant.shrotria@mayadata.io>
2020-09-16 12:00:05 +0530

    Shell Update.
    Signed-off-by: Vedant Shrotria <vedant.shrotria@mayadata.io>

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 set -e
 path=$(pwd)
 
+
+# Booting up the Litmus-Portal 
 kubectl apply -f https://raw.githubusercontent.com/litmuschaos/litmus/master/litmus-portal/k8s-manifest.yml
 kubectl wait --for=condition=Ready pods --all -n litmus
 kubectl patch svc litmusportal-frontend-service -p '{"spec": {"type": "LoadBalancer"}}' -n litmus
