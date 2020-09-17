7e44677ddc6b4d43bd079c3fe68ae7b49c5d1a41
Vedant Shrotria <vedant.shrotria@mayadata.io>
2020-09-17 22:31:01 +0530

    Testing
    Signed-off-by: Vedant Shrotria <vedant.shrotria@mayadata.io>

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 set -e
 path=$(pwd)
 
-# Setting up cluster
+# Setting up the kubeconfig
+echo "Setting Cluster details"
+mkdir -p ~/.kube
 cat $path/.kube/config > ~/.kube/config
 cat $path/.kube/admin.conf > ~/.kube/config
-cat $path/.kube/clusters > ~/logs/clusters
-cat $path/.kube/zone > ~/logs/zone
 
 # Booting up the Litmus-Portal Setup
 kubectl apply -f https://raw.githubusercontent.com/litmuschaos/litmus/master/litmus-portal/k8s-manifest.yml
@@ -18,7 +18,6 @@ kubectl patch svc litmusportal-frontend-service -p '{"spec": {"type": "LoadBalan
 # echo "export FRONTEND_IP=$(kubectl get svc litmusportal-frontend-service -n litmus -o jsonpath="{.status.loadBalancer.ingress[0].ip}")" >> build.env
 # echo $FRONTEND_IP
 
+# Getting latest kubeconfig in cache
 cat ~/.kube/config > $path/.kube/config
 cat ~/.kube/config > $path/.kube/admin.conf
-cat ~/logs/clusters > $path/.kube/clusters
-cat ~/logs/zone > $path/.kube/zone
--- uninstall.sh
+++ uninstall.sh
@@ -1,17 +1,13 @@
 set -e
 path=$(pwd)
 
-# Setting up cluster
+# Setting up kubeconfig
 cat $path/.kube/config > ~/.kube/config
 cat $path/.kube/admin.conf > ~/.kube/config
-cat $path/.kube/clusters > ~/logs/clusters
-cat $path/.kube/zone > ~/logs/zone
 
 # Shutting down the Litmus-Portal Setup
-
 kubectl delete -f https://raw.githubusercontent.com/litmuschaos/litmus/master/litmus-portal/k8s-manifest.yml
 
+# Caching the kubeconfig
 cat ~/.kube/config > $path/.kube/config
 cat ~/.kube/config > $path/.kube/admin.conf
-cat ~/logs/clusters > $path/.kube/clusters
-cat ~/logs/zone > $path/.kube/zone
