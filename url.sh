b70ca8b1f82c4ce86ebfb1e2be64e8e5d3b8b088
Vedant Shrotria <vedant.shrotria@mayadata.io>
2020-09-18 13:16:19 +0530

    Test
    Signed-off-by: Vedant Shrotria <vedant.shrotria@mayadata.io>

--- url.sh
+++ url.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
 
-# Caching the External_IP of loadBalancer in Environment Variable for Testing
-export FRONTEND_IP=$(kubectl get svc litmusportal-frontend-service -n litmus -o jsonpath="{.status.loadBalancer.ingress[0].ip}")
-echo $FRONTEND_IP
