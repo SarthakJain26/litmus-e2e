c4e29c0411867d2355c2ca6a60160f14f3cd36b6
Vedant Shrotria <vedant.shrotria@mayadata.io>
2020-09-18 13:25:32 +0530

    Test
    Signed-off-by: Vedant Shrotria <vedant.shrotria@mayadata.io>

--- install.sh
+++ install.sh
@@ -9,7 +9,6 @@ cat $path/.kube/admin.conf > ~/.kube/config
 
 # Booting up the Litmus-Portal Setup
 kubectl apply -f https://raw.githubusercontent.com/litmuschaos/litmus/master/litmus-portal/k8s-manifest.yml
-kubectl wait --for=condition=Ready pods --all -n litmus
 kubectl patch svc litmusportal-frontend-service -p '{"spec": {"type": "LoadBalancer"}}' -n litmus
 
 # Getting latest kubeconfig in cache
