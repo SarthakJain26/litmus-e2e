a034fadf8888d8abf42335d5ae013e0bad2b4ca0
Vedant Shrotria <vedant.shrotria@mayadata.io>
2020-09-19 16:27:05 +0530

    Testing
    Signed-off-by: Vedant Shrotria <vedant.shrotria@mayadata.io>

--- install.sh
+++ install.sh
@@ -10,11 +10,24 @@ cat $path/.kube/admin.conf > ~/.kube/config
 # Booting up the Litmus-Portal Setup
 kubectl apply -f https://raw.githubusercontent.com/litmuschaos/litmus/master/litmus-portal/k8s-manifest.yml
 kubectl patch svc litmusportal-frontend-service -p '{"spec": {"type": "LoadBalancer"}}' -n litmus
-kubectl wait --for=condition=Ready pods --all --namespace litmus --timeout=120s
+kubectl wait --for=condition=Ready pods --all --namespace litmus --timeout=180s
+kubectl get svc -n litmus
+
+# Wait using ping
+
 
 # Getting latest kubeconfig in cache
 cat ~/.kube/config > $path/.kube/config
 cat ~/.kube/config > $path/.kube/admin.conf
+
+external_ip=""; 
+while [ -z $external_ip ]; 
+do echo "Waiting for end point..."; 
+external_ip=$(kubectl get svc litmusportal-frontend-service -n litmus --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}"); 
+[ -z "$external_ip" ] && sleep 10; 
+done; 
+echo "End point ready-" && echo $external_ip; 
+export endpoint=$external_ip
 
 # Caching the External_IP of loadBalancer in Environment Variable for Testing
 FRONTEND_IP=$(kubectl get svc litmusportal-frontend-service -n litmus -o jsonpath="{.status.loadBalancer.ingress[0].ip}")
