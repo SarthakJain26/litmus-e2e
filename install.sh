9e5fc6832c1d8cdaeb15569536097c35802002b5
Vedant Shrotria <vedant.shrotria@mayadata.io>
2020-09-19 17:08:35 +0530

    Testing
    Signed-off-by: Vedant Shrotria <vedant.shrotria@mayadata.io>

--- install.sh
+++ install.sh
@@ -11,10 +11,12 @@ cat $path/.kube/admin.conf > ~/.kube/config
 kubectl apply -f https://raw.githubusercontent.com/litmuschaos/litmus/master/litmus-portal/k8s-manifest.yml
 kubectl patch svc litmusportal-frontend-service -p '{"spec": {"type": "LoadBalancer"}}' -n litmus
 kubectl wait --for=condition=Ready pods --all --namespace litmus --timeout=180s
+
+echo "Pods Running in Litmus Namespace"
+kubectl get pods -n litmus
+
+echo "Services Running in Litmus Namespace"
 kubectl get svc -n litmus
-
-# Wait using ping
-
 
 # Getting latest kubeconfig in cache
 cat ~/.kube/config > $path/.kube/config
@@ -26,10 +28,9 @@ do echo "Waiting for end point...";
 external_ip=$(kubectl get svc litmusportal-frontend-service -n litmus --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}"); 
 [ -z "$external_ip" ] && sleep 10; 
 done; 
-echo "End point ready-" && echo $external_ip; 
-export endpoint=$external_ip
+echo "End point is ready-" && echo $external_ip; 
 
 # Caching the External_IP of loadBalancer in Environment Variable for Testing
-FRONTEND_IP=$(kubectl get svc litmusportal-frontend-service -n litmus -o jsonpath="{.status.loadBalancer.ingress[0].ip}")
-echo $FRONTEND_IP
-echo "FRONTEND_IP=$FRONTEND_IP" >> Portal-Setup.env
+# FRONTEND_IP=$(kubectl get svc litmusportal-frontend-service -n litmus -o jsonpath="{.status.loadBalancer.ingress[0].ip}")
+# echo $FRONTEND_IP
+echo "FRONTEND_IP=$external_ip" >> Portal-Setup.env
