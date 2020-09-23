c896695e72cdaa6bbe5ed39a82e234f06c0b154d
Saranya-jena <saranya.jena@mayadata.io>
2020-09-23 23:55:31 +0530

    Added e2e tests for my accounts
    Signed-off-by: Saranya-jena <saranya.jena@mayadata.io>

--- CypressE2E/cypress/integration/Parallel_Tests/Account_Settings/Account_Settings.spec.js
+++ CypressE2E/cypress/integration/Parallel_Tests/Account_Settings/Account_Settings.spec.js
@@ -1,20 +1,102 @@
 /// <reference types="Cypress" />
 describe("Testing the My accounts section", () => {
-
-  beforeEach("Clearing local storage",()=>{
-    indexedDB.deleteDatabase('localforage');
+  before("Clearing local storage", () => {
+    cy.clearCookie("token");
+    indexedDB.deleteDatabase("localforage");
     cy.server();
-    cy.visit('/login');
-    cy.route('POST','/login').as('loginResponse'); //Alias for Login Route
-    cy.login("admin","litmus"); 
-    cy.wait('@loginResponse').its('status').should('eq',200); //Request Done.
-    cy.contains('Congratulations').should('be.visible'); //confirmation of HomePage loaded.
-  })
-
-  it("Checking the accessibility of the Settings", () => {
-    cy.visit("/settings");
-    cy.url().should("contain", "/settings");
-    cy.contains("Settings").should("be.visible");
+    cy.visit("/login");
+    cy.route("POST", "/login").as("loginResponse"); //Alias for Login Route
+    cy.login("admin", "litmus");
+    cy.wait("@loginResponse").its("status").should("eq", 200); //Request Done.
+    cy.contains("Congratulations").should("be.visible"); //confirmation of HomePage loaded.
   });
 
+  it("Checking the accessibility of the Settings", () => {
+    cy.contains("Settings").click();
+    cy.contains("My Account").should("be.visible");
+  });
+
+  it("Checking the accessibility of My Accounts", () => {
+    cy.get("[data-cy=my-account]").click();
+    cy.contains("Personal Details").should("be.visible");
+  });
+
+  it("Changing the personal details by inputting all details", () => {
+    cy.server();
+    cy.route("POST", "/update/details").as("detailsResponse");
+    cy.get("[data-cy=InputName] input").clear().type("Saranya J29");
+    cy.get("[data-cy=InputEmail] input").clear().type("saranya29@gmail.com");
+    cy.get("[data-cy=save]").click();
+    cy.wait("@detailsResponse").its("status").should("eq", 200); //Request Done.
+    cy.modalClose();
+    cy.get("[data-cy=done]").should("not.exist");
+    cy.headerCheck("Saranya J29", "saranya29@gmail.com");
+  });
+
+  it("Changing the personal details with empty email field", () => {
+    cy.server();
+    cy.route("POST", "/update/details").as("detailsResponse");
+    cy.get("[data-cy=InputName] input").clear().type("Saranya J");
+    cy.get("[data-cy=InputEmail] input").clear();
+    cy.get("[data-cy=save]").click();
+    cy.wait("@detailsResponse").its("status").should("eq", 200); //Request Done.
+    cy.modalClose();
+    cy.headerCheck("Saranya J", "");
+  });
+
+  it("Changing the personal details with empty fullname field", () => {
+    cy.get("[data-cy=InputName] input").clear();
+    cy.get("[data-cy=InputEmail] input").clear().type("saranya@gmail.com");
+    cy.get("[data-cy=save] button").should("be.disabled");
+  });
+
+  it("Changing the password by inputting all the three password fields", () => {
+    cy.changePassword("litmus", "litmus@123", "litmus@123");
+    cy.wait("@passwordResponse").its("status").should("eq", 200); //Request Done.
+    cy.modalClose();
+    cy.get("[data-cy=done]").should("not.exist");
+    cy.logout();
+    cy.url().should("include", "/login");
+    cy.server();
+    cy.route("POST", "/login").as("loginResponse"); //Alias for Login Route
+    cy.login("admin", "litmus@123");
+    cy.wait("@loginResponse").its("status").should("eq", 200); //Request Done.
+    cy.contains("Congratulations").should("be.visible"); //confirmation of HomePage loaded.
+    cy.log("Reverting back the password to litmus");
+    cy.contains("Settings").click();
+    cy.changePassword("litmus@123", "litmus", "litmus");
+    cy.wait("@passwordResponse").its("status").should("eq", 200); //Request Done.
+    cy.modalClose();
+  });
+
+  it("Changing the password by inputting any two/none of the three password fields", () => {
+    cy.log("with empty current password");
+    cy.changePassword("", "litmus@123", "litmus@123");
+    cy.get("[data-cy=change-password] button").should("be.disabled");
+    cy.log("with empty new password");
+    cy.changePassword("litmus@123", "", "litmus");
+    cy.get("[data-cy=change-password] button").should("be.disabled");
+    cy.log("with empty confirm password");
+    cy.changePassword("litmus", "litmus@123", "");
+    cy.get("[data-cy=change-password] button").should("be.disabled");
+    cy.log("with all empty fields");
+    cy.changePassword("", "", "");
+    cy.get("[data-cy=change-password] button").should("be.disabled");
+  });
+
+  it("Changing the password by inputting incorrect current password", () => {
+    cy.changePassword("abc", "litmus@123", "litmus@123");
+    cy.wait("@passwordResponse").its("status").should("eq", 401); //Request Done.
+    cy.contains("Error").should("be.visible");
+    cy.modalClose();
+    cy.log(
+      "User should be able to log in with older credentials as password did not change"
+    );
+    cy.logout();
+    cy.server();
+    cy.route("POST", "/login").as("loginResponse"); //Alias for Login Route
+    cy.login("admin", "litmus");
+    cy.wait("@loginResponse").its("status").should("eq", 200); //Request Done.
+    cy.contains("Congratulations").should("be.visible"); //confirmation of HomePage loaded.
+  });
 });
--- CypressE2E/cypress/support/commands.js
+++ CypressE2E/cypress/support/commands.js
@@ -3,58 +3,107 @@
 
 /* loginServer() command for stubbing a server 
 with /auth/login route with any status code and login details provided as argument.*/
-Cypress.Commands.add('loginServer',(loginStatus,email,password)=>{
-    cy.server();
-    cy.route({
-        method: 'POST',
-        url: Cypress.env('baseUrl')+'/login',
-        status: loginStatus,
-        response: (loginStatus==503)?'':{} 
-    }).as('login');
-    cy.login(email,password);
-    cy.wait('@login');
+Cypress.Commands.add("loginServer", (loginStatus, email, password) => {
+  cy.server();
+  cy.route({
+    method: "POST",
+    url: Cypress.env("baseUrl") + "/login",
+    status: loginStatus,
+    response: loginStatus == 503 ? "" : {},
+  }).as("login");
+  cy.login(email, password);
+  cy.wait("@login");
 });
 
 /* modalServer() command for stubbing a server 
 with /auth/update route with any status code provided as argument.*/
-Cypress.Commands.add('modalServer',(modalStatus,ProjectName,Name,Email,Password)=>{
+Cypress.Commands.add(
+  "modalServer",
+  (modalStatus, ProjectName, Name, Email, Password) => {
     cy.server();
     cy.route({
-        url:'/update/details',
-        method: 'POST',
-        status: modalStatus,
-        response: (modalStatus==503)?'':{email: "Vedant@Gmail.com",name: "Vedant"}
-    }).as('updateDetails');
-    cy.welcomeModalInputs(ProjectName,Name,Email,Password);
-    cy.wait('@updateDetails');
-})
+      url: "/update/details",
+      method: "POST",
+      status: modalStatus,
+      response:
+        modalStatus == 503 ? "" : { email: "Vedant@Gmail.com", name: "Vedant" },
+    }).as("updateDetails");
+    cy.welcomeModalInputs(ProjectName, Name, Email, Password);
+    cy.wait("@updateDetails");
+  }
+);
 
 // Custom command for Inputting details in Welcome Modal.
-Cypress.Commands.add('welcomeModalInputs',(ProjectName,Name,Password,Email)=>{
-    cy.get('[data-cy=InputProjectName] input').type(ProjectName);
-    cy.get('[data-cy=Continue]').click();
-    cy.get('[data-cy=InputName] input').type(Name)
-    cy.get('[data-cy=startButton]').eq(0).click();
-    cy.get('[data-cy=InputPassword] input').each(($div)=>{
-        cy.wrap($div).type(Password);
+Cypress.Commands.add(
+  "welcomeModalInputs",
+  (ProjectName, Name, Password, Email) => {
+    cy.get("[data-cy=InputProjectName] input").type(ProjectName);
+    cy.get("[data-cy=Continue]").click();
+    cy.get("[data-cy=InputName] input").type(Name);
+    cy.get("[data-cy=startButton]").eq(0).click();
+    cy.get("[data-cy=InputPassword] input").each(($div) => {
+      cy.wrap($div).type(Password);
     });
-    cy.get('[data-cy=startButton]').eq(0).click();
-    cy.get('[data-cy=InputEmail]').type(Email);
-    cy.get('[data-cy=startButton]').eq(0).click();
-})
+    cy.get("[data-cy=startButton]").eq(0).click();
+    cy.get("[data-cy=InputEmail]").type(Email);
+    cy.get("[data-cy=startButton]").eq(0).click();
+  }
+);
 
 //Custom command for Inputting Login Details.
-Cypress.Commands.add('login',(Username,Password)=>{
-    cy.get('[data-cy=inputName] input').type(Username);
-    cy.get('[data-cy=inputPassword] input').type(Password);
-    cy.get('[data-cy=loginButton]').click();
-})
+Cypress.Commands.add("login", (Username, Password) => {
+  cy.get("[data-cy=inputName] input").type(Username);
+  cy.get("[data-cy=inputPassword] input").type(Password);
+  cy.get("[data-cy=loginButton]").click();
+});
 
 //Custom command to Reset The database
-Cypress.Commands.add('resetDatabase',()=>{
-    // cy.exec('chmod 755 ./Scripts/resetDB.sh');
-    // cy.exec('sudo ./Scripts/resetDB.sh');
-    cy.exec('docker exec -i mongodb bash',{failOnNonZeroExit: false});
-    cy.exec('mongo -host localhost -port 27017',{failOnNonZeroExit: false});
-    cy.exec('use litmus',{failOnNonZeroExit: false});
-})
+Cypress.Commands.add("resetDatabase", () => {
+  // cy.exec('chmod 755 ./Scripts/resetDB.sh');
+  // cy.exec('sudo ./Scripts/resetDB.sh');
+  cy.exec("docker exec -i mongodb bash", { failOnNonZeroExit: false });
+  cy.exec("mongo -host localhost -port 27017", { failOnNonZeroExit: false });
+  cy.exec("use litmus", { failOnNonZeroExit: false });
+});
+
+//Custom command for Logut.
+Cypress.Commands.add("logout", () => {
+  cy.get("[data-cy=header-dropdown]").click();
+  cy.get("[data-cy=logout]").click();
+});
+
+//Custom command for changing password.
+Cypress.Commands.add(
+  "changePassword",
+  (currPassword, newPassword, confPasword) => {
+    cy.server();
+    cy.route("POST", "/update/password").as("passwordResponse");
+    currPassword === ""
+      ? cy.get("[data-cy=currPassword] input").clear()
+      : cy.get("[data-cy=currPassword] input").clear().type(currPassword);
+    newPassword === ""
+      ? cy.get("[data-cy=newPassword] input").clear()
+      : cy.get("[data-cy=newPassword] input").clear().type(newPassword);
+    confPasword === ""
+      ? cy.get("[data-cy=confPasword] input").clear()
+      : cy.get("[data-cy=confPasword] input").clear().type(confPasword);
+    cy.get("[data-cy=change-password]").click();
+  }
+);
+
+//Custom command for closing modal.
+Cypress.Commands.add("modalClose", () => {
+  cy.get("[data-cy=done]").should("be.visible");
+  cy.get("[data-cy=done]").click();
+});
+
+//Custom command for checking header details.
+Cypress.Commands.add("headerCheck", (name, email) => {
+  cy.get("[data-cy=full-name]").should("contain", name);
+  cy.get("[data-cy=header-dropdown]").click();
+  cy.get("[data-cy=name-header]").should("contain", name);
+  email === ""
+    ? cy.get("[data-cy=email-header]").should("contain", "")
+    : cy.get("[data-cy=email-header]").should("contain", email);
+  cy.root().click();
+});
